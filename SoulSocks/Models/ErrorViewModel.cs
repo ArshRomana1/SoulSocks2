e7335375697c13c4caf18e66074eaa81307c2e4b
W0791162 <W0791162@WDSOL0052Z-27>
2023-02-16 21:25:27 -0500

    Add project files.

---
+++ SoulSocks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoulSocks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
